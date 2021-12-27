b7ebe0c8893cb97340510ed36e992cd26c2513bb
huaman <zhouhuaman@std.uestc.edu.cn>
2021-12-27 21:46:32 +0800

    with NBSync

---
+++ 3rdparty/ps-lite/build/meta.pb.d
@@ -0,0 +1,15 @@
+build/meta.pb.o: src/meta.pb.cc src/meta.pb.h \
+ /home/homan/NBSync/apache-mxnet-src-1.4.0-incubating/deps/include/google/protobuf/stubs/common.h \
+ /home/homan/NBSync/apache-mxnet-src-1.4.0-incubating/deps/include/google/protobuf/generated_message_util.h \
+ /home/homan/NBSync/apache-mxnet-src-1.4.0-incubating/deps/include/google/protobuf/message_lite.h \
+ /home/homan/NBSync/apache-mxnet-src-1.4.0-incubating/deps/include/google/protobuf/repeated_field.h \
+ /home/homan/NBSync/apache-mxnet-src-1.4.0-incubating/deps/include/google/protobuf/stubs/type_traits.h \
+ /home/homan/NBSync/apache-mxnet-src-1.4.0-incubating/deps/include/google/protobuf/stubs/template_util.h \
+ /home/homan/NBSync/apache-mxnet-src-1.4.0-incubating/deps/include/google/protobuf/extension_set.h \
+ /home/homan/NBSync/apache-mxnet-src-1.4.0-incubating/deps/include/google/protobuf/stubs/once.h \
+ /home/homan/NBSync/apache-mxnet-src-1.4.0-incubating/deps/include/google/protobuf/stubs/atomicops.h \
+ /home/homan/NBSync/apache-mxnet-src-1.4.0-incubating/deps/include/google/protobuf/stubs/platform_macros.h \
+ /home/homan/NBSync/apache-mxnet-src-1.4.0-incubating/deps/include/google/protobuf/stubs/atomicops_internals_x86_gcc.h \
+ /home/homan/NBSync/apache-mxnet-src-1.4.0-incubating/deps/include/google/protobuf/io/coded_stream.h \
+ /home/homan/NBSync/apache-mxnet-src-1.4.0-incubating/deps/include/google/protobuf/wire_format_lite_inl.h \
+ /home/homan/NBSync/apache-mxnet-src-1.4.0-incubating/deps/include/google/protobuf/wire_format_lite.h
